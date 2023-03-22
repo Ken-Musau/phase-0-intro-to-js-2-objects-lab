6c8250d07599242004de94b9e7c84452bd18583a
Kennedy Musau <kennedy.musauu@gmail.com>
2023-03-22 06:04:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = { name: "Kennedy", streetAdress: "Kitengela" };
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmpl = { ...employee };
+  newEmpl[key] = value;
+  return newEmpl;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmpl = { ...employee };
+  delete newEmpl[key];
+  return newEmpl;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
